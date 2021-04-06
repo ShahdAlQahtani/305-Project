50865eb6c82f38c54269e69f5fefe5cd91496519
ShahdAlqahtani <salqahtani0432@stu.kau.edu.sa>
2021-04-07 01:04:35 +0300

    ReserveDone

--- src/project_305/TenantPage_ReserDone.java
+++ src/project_305/TenantPage_ReserDone.java
@@ -4,11 +4,18 @@
  * and open the template in the editor.
  */
 package project_305;
+import java.awt.Desktop;
+import java.io.FileNotFoundException;
+import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-/**
- *
- * @author shahad
- */
 public class TenantPage_ReserDone extends javax.swing.JFrame {
 
     /**
@@ -50,7 +57,7 @@ public class TenantPage_ReserDone extends javax.swing.JFrame {
             }
         });
         jPanel1.add(jButton1);
-        jButton1.setBounds(30, 510, 122, 42);
+        jButton1.setBounds(90, 450, 122, 42);
 
         jLabel2.setFont(new java.awt.Font("Times New Roman", 0, 24)); // NOI18N
         jLabel2.setText("Confirmed!");
@@ -92,11 +99,18 @@ public class TenantPage_ReserDone extends javax.swing.JFrame {
         jPanel1.add(jPanel2);
         jPanel2.setBounds(30, 280, 240, 130);
 
+        jButton2.setFont(new java.awt.Font("Times New Roman", 0, 11)); // NOI18N
         jButton2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/project_305/print.png"))); // NOI18N
+        jButton2.setText("print invoice");
         jButton2.setBorderPainted(false);
         jButton2.setContentAreaFilled(false);
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
         jPanel1.add(jButton2);
-        jButton2.setBounds(210, 510, 60, 50);
+        jButton2.setBounds(10, 550, 140, 30);
 
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/project_305/doneReserve.jpeg"))); // NOI18N
         jPanel1.add(jLabel1);
@@ -122,6 +136,82 @@ public class TenantPage_ReserDone extends javax.swing.JFrame {
         ob.setVisible(true);
         this.setVisible(false);
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        try {
+            // TODO add your handling code here:
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            String ConnectionURL = "jdbc:mysql://localhost:3306/weddinghallreservation";
+            PreparedStatement q;
+            Connection connection = DriverManager.getConnection(ConnectionURL, "root", "1212");
+
+            String queryT = "Select `firstname`, `Email` from `Tenant` where  `idTenant` ='" + Login.Id + "' ";
+            Statement stm = connection.createStatement();
+            ResultSet rs = stm.executeQuery(queryT);
+            
+            File file= new File("invoice.txt");
+            PrintWriter fr= new PrintWriter(file);
+            
+            
+            fr.println("|——————————INVOICE—————————-——|");
+            fr.println("|                                                                                  |");
+            fr.println("|———————WEDDING HALL SYSTEM———————|");
+            fr.println("|                                                                                  |");
+            fr.println("|--------------------------------------------|");
+            fr.println("|                                                                                  |");
+           
+           
+            while (rs.next()) {
+                String name= rs.getString(1);
+                String email= rs.getString(2);
+            fr.printf("| Tenant Info:        Name:%-39s|\n", name);
+            fr.printf("|                            Email:%-38s|\n", email);
+            }
+            
+            fr.println("|                                                                                  |");
+            
+            String queryH = "Select `Hallname`, `Hallcapacity`, `HallPrice` from `hallinfo` where `idHallInfo`='" + TenantPage_Hallinfo.id + "' ";
+            Statement stmH = connection.createStatement();
+            ResultSet rsH = stmH.executeQuery(queryH);
+
+            if (rsH.next()) {
+            fr.printf("| Reservation Info:  Hall Name: %-31s|\n", rsH.getString(1));
+            fr.printf("|                            Capacity:%-37d|\n", rsH.getInt(2));
+            fr.printf("|                            Price:%-41d|\n", rsH.getInt(3));
+            }
+            
+            fr.printf("|                            Date:%-36s|\n", TenantPage_Reservation.res.getReserveDate().toString());
+            
+           
+            
+            if (TenantPage_Reservation.res.getPayment().equals("0")) {
+                fr.printf("|                            Payment Method:%-24s|\n", "Cash");
+            } else {
+                fr.printf("|                            Payment Method:%-24s|\n", "Cridit card");
+            }
+            
+
+            
+            fr.close();
+           // fr.flush();
+            
+             Desktop desktop = Desktop.getDesktop();
+            if(file.exists()) desktop.open(file);
+            
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(TenantPage_ReserDone.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(TenantPage_ReserDone.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(TenantPage_ReserDone.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(TenantPage_ReserDone.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
